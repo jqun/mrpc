f08976adf18dabbabfb307330aea31013326da68
jqun <maqunjiang@gmail.com>
2022-10-21 18:38:42 +0800

    server

--- server.go
+++ server.go
@@ -1,7 +1,10 @@
 package mrpc
 
 import (
+	"log"
+	"mrpc/codec"
 	"mrpc/serializer"
+	"net"
 	"net/rpc"
 )
 
@@ -22,5 +25,24 @@ func NewServer(opts ...Option) *Server {
 	return &Server{
 		Server:     &rpc.Server{},
 		Serializer: options.serializer,
+	}
+}
+
+func (s *Server) Register(rcvr interface{}) error {
+	return s.Server.Register(rcvr)
+}
+
+func (s *Server) RegisterName(name string, rcvr interface{}) error {
+	return s.Server.RegisterName(name, rcvr)
+}
+
+func (s *Server) Serve(ln net.Listener) {
+	log.Printf("mrpc started on: %v", ln.Addr().String())
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			continue // 此处可以考虑休眠Duration
+		}
+		go s.Server.ServeCodec(codec.NewServerCodec(conn, s.Serializer))
 	}
 }
